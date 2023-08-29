5a6468e343a08309e9966be78e4a0eab01c25aa2
pandareedear <pandaree_c51@gmail.com>
2023-08-29 15:21:16 +0700

    finish: step4 add layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import "./App.scss";
+
 function App() {
-  return <div>React-APP</div>;
+  return (
+    <div className="todo">
+      <div className="todo__header">Header</div>
+      <div className="todo__sidebar">SideBar</div>
+      <div className="todo__content">TodoContent</div>
+    </div>
+  );
 }
 
 export default App;
